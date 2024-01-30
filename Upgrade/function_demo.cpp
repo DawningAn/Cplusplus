0c40fbe2adcaa39ce81853e72392f15e382568f7
DawningAn <3323194808@qq.com>
2024-01-30 15:25:17 +0800

    func

--- Upgrade/function_demo.cpp
+++ Upgrade/function_demo.cpp
@@ -1,1 +1,20 @@
 #include<iostream>
+#include <cmath>
+
+using namespace std;
+
+// 定义函数原型
+void calArea(double);
+const double PI {3.1415926};
+
+int main(){
+    int radious{};
+    cout << "请输入半径："<<endl;
+    cin>>radious;
+    calArea(radious);
+    return 0;
+}
+
+void calArea(double radius){
+    cout<<"圆的面积为："<< PI * pow(radius,2)<<endl;
+}
